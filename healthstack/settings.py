0e41b79029062215a5ddae052eddc2cb9be8f63d
didin <didinpg8080@gmail.com>
2025-08-04 16:55:45 +0530

    didin

--- healthstack/settings.py
+++ healthstack/settings.py
@@ -28,7 +28,7 @@ environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('1zbl!eae5k492@9aqu17l%djh*783*d+h4tw-bqx2bfd4sneco')
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
